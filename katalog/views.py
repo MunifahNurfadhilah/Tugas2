2481d5ea4a7dc7c0cc2129eee6cd38fc062bb10e
munifah.nurfadhilah <munifah.nurfadhilah@ui.ac.id>
2022-09-09 18:55:04 +0700

    yeay1

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+    'list_katalog': data_barang_katalog,
+    'nama': 'pudil',
+    'npm' : '2106654851'
+}
+    return render(request, "katalog.html", context)
