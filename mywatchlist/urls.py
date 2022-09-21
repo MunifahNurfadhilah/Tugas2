946acdeaf8250778820c8c2ac84c838159cc9143
munifah.nurfadhilah <munifah.nurfadhilah@ui.ac.id>
2022-09-21 08:36:28 +0700

    add xml json

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -1,9 +1,14 @@
 # TODO: Implement Routings Here
 from django.urls import path
 from mywatchlist.views import show_mywatchlist
+from mywatchlist.views import show_xml
+from mywatchlist.views import show_json
 
 app_name = 'mywatchlist'
 
 urlpatterns = [
     path('', show_mywatchlist, name='show_mywatchlist'),
+    path('html/', show_mywatchlist, name='show_mywatchlist'),
+    path('xml/', show_xml, name='show_xml'), 
+    path('json/', show_json, name='show_json'),
 ]
--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from mywatchlist.models import MyWatchList
+from django.http import HttpResponse
+from django.core import serializers
 
 # Create your views here.
 def show_mywatchlist(request):
@@ -10,3 +12,11 @@ def show_mywatchlist(request):
     'npm' : '2106654851'
 }
     return render(request, "mywatchlist.html", context)
+
+def show_xml(request):
+    data = MyWatchList.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = MyWatchList.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
