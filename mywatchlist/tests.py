4778a70fa6d619d6e79259ef657c5fc66fbc280d
munifah.nurfadhilah <munifah.nurfadhilah@ui.ac.id>
2022-09-21 13:01:11 +0700

    unit testing

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from urllib import response
+from django.test import TestCase, Client
+from django.urls import resolve
 
 # Create your tests here.
+class TestUrl(TestCase):
+    def test_url_html_exists(self):
+        response = Client().get('http://localhost:8000/mywatchlist/html/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_url_xml_exists(self):
+        response = Client().get('http://localhost:8000/mywatchlist/xml/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_url_json_exists(self):
+        response = Client().get('http://localhost:8000/mywatchlist/json/')
+        self.assertEqual(response.status_code, 200)
+    
--- project_django/settings.py
+++ project_django/settings.py
@@ -133,7 +133,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 for directory in [*STATICFILES_DIRS, STATIC_ROOT]:
     directory.mkdir(exist_ok=True)
